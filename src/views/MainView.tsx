cb768f898ad9ad27475471a3c60ac993f9d54632
PK <koszelak.piotr@gmail.com>
2024-11-09 00:13:23 +0100

    Refactor main view

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,8 +1,10 @@
 import { menuHeight } from '../utils/constants';
 
-import { Box, styled } from '@mui/material';
+import { Box, Paper, styled } from '@mui/material';
 import Calculator from '../components/calculator/Calculator';
 import Sidebar from '../components/layout/Sidebar';
+import Panels from '../components/layout/panels/Panels';
+import { useIsMobile } from '../hooks/common';
 
 const StyledView = styled(Box)`
     width: 100%;
@@ -10,11 +12,27 @@ const StyledView = styled(Box)`
     display: flex;
 `;
 
-const MainView = () => (
-    <StyledView>
-        <Calculator isSingle />
-        <Sidebar />
-    </StyledView>
-);
+const StyledPanel = styled(Paper)`
+    width: 500px;
+`;
+
+const MainView = () => {
+    const isMobile = useIsMobile();
+
+    return (
+        <StyledView>
+            <Calculator isSingle />
+            {isMobile ? (
+                <Sidebar>
+                    <Panels />
+                </Sidebar>
+            ) : (
+                <StyledPanel>
+                    <Panels />
+                </StyledPanel>
+            )}
+        </StyledView>
+    );
+};
 
 export default MainView;
